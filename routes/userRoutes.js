3704df3deff0e545d974d46f3fec817d5a3c7ec8
cjenschke <cimarron13@mac.com>
2023-12-08 15:52:13 -0600

    add/delete friends

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -59,4 +59,51 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+//POST to add a new friend to a user's friend list
+router.post('/:userId/friends/:friendsId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    const friend = await User.findById(req.params.friendsId);
+
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    //Prevent adding the same friend twice
+    if (user.friends.includes(req.params.friendId)) {
+      return res.status(400).json({ message: 'Friend already added' });
+    }
+
+    user.friends.push(req.params.friendsId);
+    await user.save();
+
+    res.json({ message: 'Friend added successfully', user });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: 'Internal Server Error', error: err.message });
+  }
+});
+
+// DELETE to remove a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    user.friends = user.friends.filter(
+      (friendId) => friendId.toString() !== req.params.friendId
+    );
+
+    await user.save();
+
+    res.json({ message: 'Friend removed successfully', user });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
