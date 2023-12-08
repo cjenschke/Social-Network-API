8adfd30cb49fd9e7a466ab072e5303d3bc06c7b7
cjenschke <cimarron13@mac.com>
2023-12-08 14:24:22 -0600

    fixed seed issue.  DB now seeded

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { User, Thought } = require('./models');
+const { User, Thought } = require('../models');
 
 mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB');
 
