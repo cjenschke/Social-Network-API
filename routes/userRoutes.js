20eca03977e2e794d8b7bf583af5820a91d3ce01
cjenschke <cimarron13@mac.com>
2023-12-08 14:00:32 -0600

    routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { User, Thought } = require('../models');
+
+// GET all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET a single user by its id and populated thought and friend data
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate('thoughts')
+      .populate('friends');
+    if (!user) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+    res.json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//POST a new user
+router.post('/', async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//PUT to update a user by its id
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE to remove user by its id
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    res.json(deletedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
